36ad913ccd068f09aafb69ad5cb7a99822c34dcf
Lê Alves <leticiaviz.alves@gmail.com>
2024-02-05 22:31:55 -0300

    primeiro

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from ucimlrepo import fetch_ucirepo 
+from ucimlrepo import fetch_ucirepo
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
   
 # fetch dataset 
 spambase = fetch_ucirepo(id=94) #Utilização da função "fetch..."para baixar o dataset de ID 94
@@ -13,3 +23,6 @@ y = spambase.data.targets
 # variable information 
 #print(spambase.variables) 
 #-----------------------------------------------------------------------------------------------------------
+print(X.describe())
+print ("----------------------------------------------------------------")
+print (y.value_counts())
